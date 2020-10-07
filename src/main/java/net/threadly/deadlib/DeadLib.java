a7f778b85f23f95014cc785adca9b8eb14652f7e
threadlly <victorbetoni@protonmail.com>
2020-10-07 18:08:01 -0300

    Initial commit

---
+++ src/main/java/net/threadly/deadlib/DeadLib.java
@@ -0,0 +1,26 @@
+package net.threadly.deadlib;
+
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "deadlib",
+        name = "DeadLib",
+        authors = {
+                "threadly"
+        }
+)
+public class DeadLib {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+    }
+}
